5cdbcbea12a3eb3be2fb1d5e57699c1d7edbabbe
Diaconu Alexandru-Gabriel <gabrieldiaconu16@gmail.com>
2025-01-05 21:16:11 +0200

    'am modificat acum lucrez cu sklearn'

--- test.py
+++ test.py
@@ -1,31 +1,55 @@
 import pandas as pd
-from pycaret.classification import *
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+import numpy as np
 
-def categorize_pm25(value):
-    if value < 12.5:
-        return 'Good'
-    elif 12.5 <= value < 25:
-        return 'Fair'
-    elif 25 <= value < 50:
-        return 'Poor'
-    elif 50 <= value < 150:
-        return 'Very poor'
-    else:
-        return 'Extremely poor'
-    
-pm25 = pd.read_excel('C:/Users/pc/Desktop/pm2.5.xlsx')
-# print(pm25)
+# Citirea datelor din CSV
+data = pd.read_excel('C:/Users/pc/Desktop/date/datetoate.xlsx')
 
-pm25['air_quality_category'] = pm25['median'].apply(categorize_pm25)
+# Selectarea caracteristicilor și a țintei
+features = ['co2_min', 'co2_max', 'co2_median', 'co2_q1', 'co2_q3', 'co2_stdev', 'co2_count',
+            'rh_min', 'rh_max', 'rh_median', 'rh_q1', 'rh_q3', 'rh_stdev', 'rh_count',
+            'presiune_min', 'presiune_max', 'presiune_median', 'presiune_q1', 'presiune_q3', 'presiune_stdev', 'presiune_count',
+            'temperatura_min', 'temperatura_max', 'temperatura_median', 'temperatura_q1', 'temperatura_q3', 'temperatura_stdev', 'temperatura_count',
+            'o3_min', 'o3_max', 'o3_median', 'o3_q1', 'o3_q3', 'o3_stdev', 'o3_count',
+            'pm1_min', 'pm1_max', 'pm1_median', 'pm1_q1', 'pm1_q3', 'pm1_stdev', 'pm1_count',
+            'pm10_min', 'pm10_max', 'pm10_median', 'pm10_q1', 'pm10_q3', 'pm10_stdev', 'pm10_count']
+x = data[features]
+y = data['pm2.5_median']
 
-print(pm25['air_quality_category'].value_counts())
+# Împărțirea datelor în seturi de antrenament și testare
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-clf1 = setup(data=pm25, target='air_quality_category', use_gpu=True)
+# Antrenarea modelelor de regresie
+models = {
+    'RandomForest': RandomForestRegressor(),
+    'LinearRegression': LinearRegression(),
+    'DecisionTree': DecisionTreeRegressor()
+}
 
-best = compare_models()
+# Evaluarea și compararea modelelor
+results = {}
+for name, model in models.items():
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    results[name] = {'MSE': mse, 'R2': r2}
+    print(f"{name} - MSE: {mse}, R2: {r2}")
 
-# evaluate_model(best)
+# Vizualizarea importanței caracteristicilor pentru RandomForest
+rf_model = models['RandomForest']
+importances = rf_model.feature_importances_
+indices = np.argsort(importances)[::-1]
 
-plot_model(best, plot = 'confusion_matrix')
-# plot_model(best, plot = 'auc')
-plot_model(best, plot = 'feature')
+# Plotarea importanței caracteristicilor
+plt.figure()
+plt.title("Importanța caracteristicilor - RandomForest")
+plt.bar(range(x.shape[1]), importances[indices], align="center")
+plt.xticks(range(x.shape[1]), x.columns[indices], rotation=90)
+plt.xlim([-1, x.shape[1]])
+plt.show()
